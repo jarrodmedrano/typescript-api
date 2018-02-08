24f0c79505727ada4d432d8f55ce77bd19ab839e
jmedran <jmedran@gmail.com>
2018-02-08 06:56:51 -0700

    add onError function

--- server/api/apiGetAllCourses.ts
+++ server/api/apiGetAllCourses.ts
@@ -1,9 +1,16 @@
 import {Request, Response} from 'express';
 import {findAllCourses} from "../queries/findAllCourses";
+import * as _ from 'lodash';
 
 export function apiGetAllCourses(req: Request, res: Response) {
 findAllCourses()
     .then(results => {
         res.status(200).json({results});
-    });
+    })
+    .catch(_.partial(onError, res, "Find all courses failed"));
+}
+
+function onError(res:Response, message: string, err:any) {
+    console.error("Promise chain error", message, err);
+    res.status(500).send();
 }
