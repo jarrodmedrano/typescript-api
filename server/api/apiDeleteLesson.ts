2956d85b3d3898221c16319c1e2db641a5c46a98
jmedran <jmedran@gmail.com>
2018-02-08 09:53:55 -0700

    adding delete api

---
+++ server/api/apiDeleteLesson.ts
@@ -0,0 +1,18 @@
+import {Request, Response} from 'express';
+import * as _ from 'lodash';
+import {onError} from "./onError";
+import {onSuccess} from "./onSuccess";
+import {databaseErrorHandler} from "./databaseErrorHandler";
+import {deleteLesson} from "../queries/deleteLesson";
+
+export function apiDeleteLesson(req:Request, res:Response) {
+    const lessonId = req.params.id;
+
+    deleteLesson(lessonId)
+        .then(_.partial(onSuccess, res))
+        .catch(err => {
+            _.partial(databaseErrorHandler, res);
+        })
+        .catch(_.partial(onError, res, `Could not delete lesson ${lessonId}`));
+}
+
