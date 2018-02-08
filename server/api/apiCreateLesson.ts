92070a704ed6d61b6e60eabcabe1a8ccba28f3b7
jmedran <jmedran@gmail.com>
2018-02-08 08:58:27 -0700

    adding better error handling for post request

--- server/api/apiCreateLesson.ts
+++ server/api/apiCreateLesson.ts
@@ -4,8 +4,15 @@ import {createLesson} from '../queries/createLesson';
 import {onError} from "./onError";
 import {onSuccess} from "./onSuccess";
 
+const hri = require('human-readable-ids').hri;
+
 export function apiCreateLesson(req:Request, res:Response) {
     createLesson(req.body)
         .then(_.partial(onSuccess, res))
+        .catch(err => {
+            const id = hri.random();
+            console.error("Database error occurred ", id, err);
+            res.status(500).json({code: 'ERR-002', message: `Creation of lesson failed, error code ${id}`});
+        })
         .catch(_.partial(onError, res, 'Could not create lesson'));
 }
