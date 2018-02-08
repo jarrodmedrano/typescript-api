20d8849cac9792a1203a751bea807151175ee2ac
jmedran <jmedran@gmail.com>
2018-02-07 19:10:15 -0700

    initialize

---
+++ server/server.ts
@@ -0,0 +1,19 @@
+import {findAllCourses} from "./queries/findAllCourses";
+import * as express from 'express';
+import {Application} from 'express';
+
+const app: Application = express();
+
+app.route('/api/courses').get( (req, res) => {
+    findAllCourses()
+        .then(results => {
+            res.status(200).json(results)
+        });
+});
+
+app.listen(8090, () => {
+    console.log('Server is running on port 8090...');
+});
+//
+// findAllCourses()
+//     .then(results => console.log(JSON.stringify(results)));
